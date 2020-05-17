363b135d038308da5322b806074379ecf8f2988a
westhouseK <nishiiekazuki@gmail.com>
2020-05-17 15:12:14 +0900

    delete duplicated code

--- app/Http/Controllers/TwitterFormController.php
+++ app/Http/Controllers/TwitterFormController.php
@@ -6,19 +6,18 @@ require base_path('vendor/autoload.php');
 use Illuminate\Http\Request;
 use App\Twitterapi;
 
-// View::makeができるようになる
-// use Illuminate\Support\Facades\View;
-
 class TwitterFormController extends Controller
 {
+  public function __construct()
+  {
+    $this->twitter = new Twitterapi;
+  }
   // main display
   public function showForm(Request $request)
   {
     $access_token = $request->session()->get('twAccessToken');
     $conection = $this->authenticateAccount($access_token);
-
-    $twitter = new Twitterapi;
-    $data = $twitter->getTwitterInfo($conection);
+    $data = $this->twitter->getTwitterInfo($conection);
 
     return view('twitter_form', compact('data'));
   }
@@ -29,9 +28,7 @@ class TwitterFormController extends Controller
     $tweet = $_POST['tweet'];
     $access_token = $request->session()->get('twAccessToken');
     $conection = $this->authenticateAccount($access_token);
-
-    $twitter = new Twitterapi;
-    $result = $twitter->sendTweet($conection, $tweet);
+    $result = $this->twitter->sendTweet($conection, $tweet);
     if (!empty($result->errors)) {
       $msg = $result->errors[0]->message;
     } else {
