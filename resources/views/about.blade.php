357c0862d191e63efbe5d79a1cd3fd2617f9954f
kazuki_nishiie <nishiiekazuki@gmail.com>
2020-08-08 16:13:00 +0900

    fix design

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,29 +2,29 @@
 
 @section('main-content')
   <div class="mb-5">
-    <h3 class="mb-3">このアプリについて</h3>
-    <u class="h5 mb-2">使用技術</u>
+    <h3 class="about-title mb-3">このアプリについて</h3>
+    <u class="about-sub h5 mb-2">使用技術</u>
     <div class="m-2">
       <table>
         <tr>
-          <td class="text-right">サーバーサイド：</td>
-          <td class="text-left">PHP(Laravel)</td>
+          <td class="about-item text-right">サーバーサイド：</td>
+          <td class="about-item text-left">PHP(Laravel)</td>
         </tr>
         <tr>
-          <td class="text-right">フロントエンド：</td>
-          <td class="text-left">JavaScript(jQuery) CSS(Bootstrap)</td>
+          <td class="about-item text-right">フロントエンド：</td>
+          <td class="about-item text-left">JavaScript(jQuery) CSS(Bootstrap)</td>
         </tr>
         <tr>
-          <td class="text-right">インフラ：</td>
-          <td class="text-left">Docker AWS</td>
+          <td class="about-item text-right">インフラ：</td>
+          <td class="about-item text-left">Docker AWS</td>
         </tr>
         <tr>
-          <td class="text-right">バージョン管理：</td>
-          <td class="text-left">GitHub</td>
+          <td class="about-item text-right">バージョン管理：</td>
+          <td class="about-item text-left">GitHub</td>
         </tr>
       </table>
     </div>
-    <u class="h5 mt-2">作成者</u>
-    <p>うえすと(<a href="https://twitter.com/westhouse_k">@westhouse_k<a>)</p>
+    <u class="about-sub h5 mt-2">作成者</u>
+    <p class="about-name">うえすと(<a href="https://twitter.com/westhouse_k">@westhouse_k<a>)</p>
   </div>
 @endsection
